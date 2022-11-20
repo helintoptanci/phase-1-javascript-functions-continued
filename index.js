f86562e9a4910b75bac37dedca1249da576a3c4e
Helin <helintoptanci3@outlook.com>
2022-11-20 18:57:05 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // code your solution here
+function saturdayFun(target = "roller-skate") {
+    console.log(`This Saturday, I want to ${target}!`);
+}
+saturdayFun("watch movies");
+
+
+
+const fn = function mondayWork(target="go to office") {
+    console.log(`This Monday, I will ${target}`);
+  }; 
+  fn(); 
+
+  function wrapAjective(snowflake= "*") {
+    return function (adj= "special") {
+      return  `You are ${adj} ${snowflake}.`;
+    
+      
+    };
+   
+  }
+  wrapAjective("!")("excellent programmer");
+  
